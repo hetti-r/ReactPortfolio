0ca0cf565b0d4450c5799e14b846b5a10f98674e
hetti-r <hetti.ronnemaa@gmail.com>
2025-02-14 13:52:51 +0200

    Fix skillcard positions, add content.

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const SkillCard = ({ imgSrc, imgAlt, barSrc, barAlt, skillName }) => {
+const SkillCard = ({ imgSrc, imgAlt, barSrc, barAlt, skillName, skillText }) => {
   return (
     <div className="skillbox">
       <img className="skillbox-img" src={imgSrc} alt={imgAlt} />
       <img className="skillbox-bar" src={barSrc} alt={barAlt} />
       <h5>{skillName}</h5>
+      <p>{skillText}</p>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -51,42 +51,48 @@ const Home = () => {
             imgAlt="sleepy pixel cat" 
             barSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/4progressbar.png" 
             barAlt="pixel progressbar half full" 
-            skillName="Game Design" 
+            skillName="Game Design"
+            skillText={"I have a strong foundation in game design, with a focus on creating engaging and immersive experiences for players. I have experience in 2D art, animation, and level design."}
           />
           <SkillCard
             imgSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/floppy.png" 
             imgAlt="pixel floppy disc" 
             barSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/2progressbar.png" 
             barAlt="pixel progressbar half full" 
-            skillName="HTML&CSS" 
-          />
-          <SkillCard
-            imgSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/folder.png" 
-            imgAlt="pixel folder with flowers" 
-            barSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/5progressbar.png" 
-            barAlt="pixel progressbar quite empty" 
-            skillName="Frontend" 
+            skillName="Backend"
+            skillText={"I am proficient in back-end technologies such as Symfony, Drupal and MySQL, which enable me to create robust and scalable server-side applications."}
           />
           <SkillCard
             imgSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/kone.png" 
             imgAlt="pixel computer with a face" 
             barSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/progressbar.png" 
             barAlt="pixel progressbar quite empty" 
-            skillName="Backend" 
+            skillName="Frontend"
+            skillText={"I focus on front-end technologies such as HTML, CSS, and JavaScript. I am passionate about creating visually stunning and user-friendly websites that engage and inspire visitors."}
+          />
+                    <SkillCard
+            imgSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/folder.png" 
+            imgAlt="pixel folder with flowers" 
+            barSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/5progressbar.png" 
+            barAlt="pixel progressbar quite empty" 
+            skillName="Art"
+            skillText={"I have a strong background in 2D art, animation, and level design, which allows me to contribute to various aspects of game development."}
           />
           <SkillCard
             imgSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/heart.png" 
             imgAlt="pixel crystal heart" 
             barSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/4progressbar.png" 
             barAlt="pixel progressbar half full" 
-            skillName="Art" 
+            skillName="Teamwork"
+            skillText={"I'm good at team building and I tend to encourage and inspire other people. While working in a team I'm also keen on open dialogue and transparency."}
           />
           <SkillCard
             imgSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/kuppi.png" 
             imgAlt="pixel cup with sprouts and a heart" 
             barSrc="https://raw.githubusercontent.com/hetti-r/hetti-r.github.io/main/ICONS/3progressbar.png" 
             barAlt="pixel progressbar almost full" 
-            skillName="Screenwriting" 
+            skillName="Screenwriting"
+            skillText={"I have experience writing for a variety of genres and formats, including feature films, television series, and short films. I am adept at crafting engaging and emotionally resonant stories that captivate and inspire audiences."}
           />
         </div>
       </section>
