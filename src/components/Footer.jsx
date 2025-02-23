1fba6a3fc72eb1fa998a0e5a5b169141f10a7c6e
Hetti <hetti.ronnemaa@gmail.com>
2025-02-23 16:33:10 +0200

    Add icons for footer and delete the contact form, edit preview pics for projects, add projects.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,40 @@
 import React from 'react'
 
 const Footer = () => {
-const currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
 
-return (
-    <footer>
-        <section id="formcontainer">
-            <div className="form-wrap">
-                <h4>Contact Me</h4>
-                <form id="contact-me">
-                    <div className="form-group">
-                        <label htmlFor="firstname">Your name</label>
-                        <input type="text" name="firstname" id="firstname" />
+    return (
+        <footer>
+            <section id="formcontainer">
+                <div className="form-wrap">
+                    <h4>Contact Me</h4>
+                    <div class="social-links">
+                        <a href="mailto:hetti.ronnemaa@gmail.com" class="social-link">
+                            <i class="fas fa-envelope"></i>
+                            <span>Email</span>
+                        </a>
+
+                        <a href="https://github.com/hetti-r" class="social-link">
+                            <i class="fab fa-github"></i>
+                            <span>GitHub</span>
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/hetti-r%C3%B6nnemaa-30986016b/" class="social-link">
+                            <i class="fab fa-linkedin"></i>
+                            <span>LinkedIn</span>
+                        </a>
+
+                        <a href="https://artstation.com/yourprofile" class="social-link">
+                            <i class="fab fa-artstation"></i>
+                            <span>ArtStation</span>
+                        </a>
                     </div>
+                </div>
+            </section>
 
-                    <div className="form-group">
-                        <label htmlFor="email">Your email</label>
-                        <input type="email" name="email" id="email" />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="message">Message</label>
-                        <input type="text" name="message" id="message" />
-                    </div>
-
-                    <button type="submit">SEND MESSAGE</button>
-                </form>
-            </div>
-        </section>
-
-        <small id="bottom">&copy; Hetti Rönnemaa {currentYear}</small>
-    </footer>
-)
+            <small id="bottom">&copy; Hetti Rönnemaa {currentYear}</small>
+        </footer>
+    )
 }
 
 export default Footer
